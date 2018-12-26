793df6c4a0fce4de699d4ce80abbb40c41c75ace
joao <joao182cx@gmail.com>
2018-12-26 14:34:02 -0200

    Adição dos gets e sets.

--- src/Data.java
+++ src/Data.java
@@ -1,18 +1,37 @@
 
 public class Data {
 	
-	public Data(int dia, int mes, int ano) {
-		
-		
+	private int dias;
+	private int ano;
+	
+	public Data(int dias, int mes, int ano) {
 		
 	}
 	
-	public Data(int dia, String mes, int ano) {
+	public Data(int dias, String mes, int ano) {
 		
 	}
 	
-	public Data(int dia, int ano) {
+	public Data(int dias, int ano) {
 		
 	}
+
+	public int getDias() {
+		return dias;
+	}
+
+	public void setDias(int dias) {
+		this.dias = dias;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	
 	
 }
