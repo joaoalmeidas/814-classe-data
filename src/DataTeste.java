91725aa49909fb839d096a0c3e979e2816bc8b50
joao <joao182cx@gmail.com>
2018-12-28 22:49:07 -0200

    Testes Finais

--- src/DataTeste.java
+++ src/DataTeste.java
@@ -7,14 +7,23 @@ public class DataTeste {
 		Data diaMesStringAno = new Data(30, "Agosto", 1945);
 		Data diaAno = new Data(60, 2013);
 		
-		diaAno.exibeFormatoDiaAno();
+		System.out.println("Primeiro construtor Data(int dia, int mes, int ano)\n");
+		
+		diaMesAno.exibeFormateDiaMesAno();
+		diaMesAno.exibeFormatoMesString();
 		diaMesAno.exibeFormatoDiaAno();
 		
-		diaMesAno.exibeFormatoMesString();
+		System.out.println("\n\nSegundo construtor Data(int dia, String mes, int ano)\n");
 		
-		diaAno.exibeFormatoMesString();
+		diaMesStringAno.exibeFormateDiaMesAno();
+		diaMesStringAno.exibeFormatoMesString();
+		diaMesStringAno.exibeFormatoDiaAno();
+		
+		System.out.println("\n\nTerceiro construtor Data(int dias, int ano)");
 		
 		diaAno.exibeFormateDiaMesAno();
+		diaAno.exibeFormatoMesString();
+		diaAno.exibeFormatoDiaAno();
 	}
 	
 }
